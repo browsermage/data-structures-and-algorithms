3c10343320f551fc26ba51aa9d0cda6be4789b93
Björn Hjorth <hjorth.bjorn@gmail.com>
2023-08-19 21:00:02 +0200

    added more tests

--- src/binary-search-list.ts
+++ src/binary-search-list.ts
@@ -1,6 +1,5 @@
 export default function bs_list(haystack: number[], needle: number): boolean
 {
-
     let low = 0;
     let high = haystack.length;
 
--- tests/bubble-sort.spec.ts
+++ tests/bubble-sort.spec.ts
@@ -5,7 +5,6 @@ test("bubble-sort", function ()
 {
   const arr = [9, 3, 7, 4, 69, 420, 42, 123, 54, 45];
 
-  // debugger;
   bubble_sort(arr);
   expect(arr).toEqual([3, 4, 7, 9, 42, 45, 54, 69, 123, 420]);
 });
